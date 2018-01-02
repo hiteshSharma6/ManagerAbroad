0922e7e2165d3c8af7910686e79bf2bc06186f86
Hitesh Sharma <hitesh6sharma@gmail.com>
2018-01-02 23:24:23 +0530

    init

--- src/com/finessy/web/login/RegistrationServlet.java
+++ src/com/finessy/web/login/RegistrationServlet.java
@@ -1,4 +1,4 @@
-package com.finessy.web.forum;
+package com.finessy.web.login;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,9 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/forum")
-public class ForumServlet extends HttpServlet {
-	
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
