ca9da799e1a819961619cec0102b82bd1f1618fb
sanuj123 <sanujs97@gmail.com>
2018-01-04 00:49:14 +0530

    Add files via upload

--- src/com/finessy/web/user/UserDTO.java
+++ src/com/finessy/web/user/UserDTO.java
@@ -2,28 +2,44 @@ package com.finessy.web.user;
 
 public class UserDTO {
 	
+	private int studentId;
 	private String firstName;
 	private String lastName;
 	private String emailId;
 	private String password;
+	private String emailHash;
+	private String status;
 	
 	
-	public UserDTO(String firstName, String lastName, String emailId, String password) {
+	public UserDTO(String firstName, String lastName, String emailId, String password, String emailHash, String status) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.emailId = emailId;
 		this.password = password;
+		this.emailHash = emailHash;
+		this.status = status;
 	}
-	
+			
+	public UserDTO(int studentId, String firstName, String lastName, String emailId, String password, String emailHash,
+			String status) {
+		this.studentId = studentId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.password = password;
+		this.emailHash = emailHash;
+		this.status = status;
+	}
+
 	public UserDTO(String emailId, String password) {
 		this.emailId = emailId;
 		this.password = password;
 	}
-
+		
 	@Override
 	public String toString() {
-		return "UserDTO [firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId + ", password="
-				+ password + "]";
+		return "UserDTO [studentId=" + studentId + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId="
+				+ emailId + ", password=" + password + ", emailHash=" + emailHash + ", status=" + status + "]";
 	}
 
 	public String getFirstName() {
@@ -58,5 +74,24 @@ public class UserDTO {
 		this.password = password;
 	}
 	
-	
+	public String getEmailHash() {
+		return emailHash;
+	}
+
+	public void setEmailHash(String emailHash) {
+		this.emailHash = emailHash;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+		
 }
