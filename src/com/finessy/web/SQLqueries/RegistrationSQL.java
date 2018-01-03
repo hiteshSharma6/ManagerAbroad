3b72966d521b3822eed4712ddc759c0c637fe7bf
sanuj123 <sanujs97@gmail.com>
2018-01-03 20:08:26 +0530

    Delete RegistrationSQL.java

--- src/com/finessy/web/SQLqueries/RegistrationSQL.java
+++
@@ -1,6 +0,0 @@
-package com.finessy.web.SQLqueries;
-
-public interface RegistrationSQL {
-  
-	String REGISTER_USER = "INSERT INTO student_login(`firstname`, `lastname`, `emailid`, `password`, `lastlogin`) VALUES(?,?,?,?,now());";
-}
